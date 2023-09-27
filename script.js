bc85ee9d3a3f9d0bc32c55ee50d5109ff50c8e73
Andrew Shoener <a.b.shoener@gmail.com>
2023-09-27 15:24:47 -0500

    Add function to ask for and validate constraints

--- script.js
+++ script.js
@@ -21,7 +21,31 @@ const constraints = {
   special: true,
 };
 
+function promptConstraints() {
+  const length = parseInt(
+    prompt("Enter password length. Must be between 8 and 128 characters.")
+  );
+  if (isNaN(length)) {
+    return "Length must be a number";
+  } else if (length < constraints.minLength || length > constraints.maxLength) {
+    return "Length must be between 8 and 128 characters.";
+  }
+  const userConstraints = {
+    special: confirm("Include special characters?"),
+    lowercase: confirm("Include lowercase characters?"),
+    uppercase: confirm("Include uppercase characters?"),
+    numbers: confirm("Include numeric characters?"),
+    length,
+  };
+  const numTrue = Object.values(userConstraints).filter(
+    (c) => typeof c == "boolean" && c
+  );
+  if (!numTrue) return "At least one character type must be selected";
+  return userConstraints;
+}
+
 function writePassword() {
+  console.log(promptConstraints());
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
