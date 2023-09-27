e247e8ed0d34e3c1fc8f1a8efefa8943720bb98f
Andrew Shoener <a.b.shoener@gmail.com>
2023-09-27 15:17:18 -0500

    Define constraints and character sets

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  special: "~!@#$%^&*()_+{}[]\\|;:'\"<>,.?/",
+};
+
 // Write password to the #password input
+const constraints = {
+  minLength: 8,
+  maxLength: 128,
+  length: 8,
+  lowercase: true,
+  uppercase: true,
+  numbers: true,
+  special: true,
+};
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
